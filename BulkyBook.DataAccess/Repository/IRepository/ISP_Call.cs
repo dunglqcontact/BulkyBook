b05a097150874a83ae82d93e61075ad60830935d
Quoc Dung Le <dunglq.contact@gmail.com>
2021-06-21 11:20:38 +0700

    Add project files.

---
+++ BulkyBook.DataAccess/Repository/IRepository/ISP_Call.cs
@@ -0,0 +1,21 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    //implement stored proc repository
+    public interface ISP_Call : IDisposable
+    {
+        T Single<T>(string procedureName, DynamicParameters param = null);
+
+        void Execute(string procedureName, DynamicParameters param = null);
+
+        T OneRecord<T>(string procedureName, DynamicParameters param = null);
+
+        IEnumerable<T> List<T>(string procedureName, DynamicParameters param = null);
+        //return 2 table
+        Tuple<IEnumerable<T1>,IEnumerable<T2>> List<T1,T2>(string procedureName, DynamicParameters param = null);
+    }
+}
